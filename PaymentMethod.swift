0409da458fc270d4007317e00a32a88928b53d7e
Jayce Sagvold <97198910+jSagvold28@users.noreply.github.com>
2022-12-18 19:44:16 -0600

    Create PaymentMethod.swift

---
+++ PaymentMethod.swift
@@ -0,0 +1,115 @@
+//
+//  PaymentMethod.swift
+//  ECommerceApp.1
+//
+//  Created by Jayce Sagvold on 12/18/22.
+//
+
+import SwiftUI
+
+struct PaymentMethod: View {
+    
+    @State private var CreditCard: Bool = false
+    
+    @State private var AppleCard: Bool = false
+    
+    @State private var RememberCard: Bool = false
+    
+    
+    
+    // Advanced Settings
+    
+    @State private var TwoFactorAuthentication: Bool = false
+    
+    @State private var DisableCreditCardEverytimeAppClosed: Bool = false
+    
+    
+    // Basic Settings
+    
+    @State private var ChangeEmailAdress: String = ""
+    
+    @State private var ChangeCreditCardNumber: String = ""
+    
+    
+    
+    
+    // Pro Settings
+    
+    @State private var EnablePro: Bool = false
+    
+    
+    
+    var body: some View {
+        
+            
+            
+            VStack {
+                
+                
+                List {
+                    
+                    
+                    // Basic Account Settings
+                    
+                    
+                    
+                    Text("Payment Method")
+                        .font(.title)
+                        .bold()
+                        .padding(.bottom, 13)
+                    
+                    Toggle("Credit card for payment", isOn: $CreditCard)
+                    
+                    Toggle("Apple Card", isOn: $AppleCard)
+                    
+                    Toggle("Remember Payment Method", isOn: $RememberCard)
+                    
+                    
+                    
+                    // Account Information
+                    
+                    Text("Basic Account Information")
+                        .font(.title2)
+                        .padding(.vertical, 5.5)
+                        .bold()
+                    
+                    TextField("Change email adress", text: $ChangeEmailAdress)
+                        .foregroundColor(.black)
+                        .keyboardType(.emailAddress)
+                    
+                    TextField("Change credit card number", text: $ChangeCreditCardNumber)
+                        .foregroundColor(.black)
+                        .keyboardType(.numberPad)
+                    
+                    
+                    // Pro Settings
+                    
+                    
+                    //    Toggle("Enable Pro", isOn: $EnablePro)
+                    
+                    
+                    
+                    // Advanced Section
+                    
+                    Text("Advanced Settings")
+                        .padding(.vertical, 5.5)
+                        .font(.title2)
+                        .bold()
+                    
+                    // 2FA
+                    
+                    Toggle("Two Factor Authentication", isOn: $TwoFactorAuthentication)
+                    
+                    Toggle("Disable Credit Card Everytime Closed", isOn: $DisableCreditCardEverytimeAppClosed)
+                    
+                    
+                }
+            }
+        }
+    }
+    
+    struct PaymentMethod_Previews: PreviewProvider {
+        static var previews: some View {
+            PaymentMethod()
+        }
+    }
